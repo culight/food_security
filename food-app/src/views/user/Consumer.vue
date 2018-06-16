789c4dec622914bd809224a16b7308afb39ea821
Gwen Faraday <gwenfaraday@gmail.com>
2018-06-16 14:12:14 -0400

    maps

--- food-app/src/views/user/Consumer.vue
+++ food-app/src/views/user/Consumer.vue
@@ -7,51 +7,64 @@
     </div>
 
     <div class="map-container large-8 cell">
-      <l-map :zoom="zoom" ref="mymap" :center="center" style="width: 100%; height: 500px" :options='{zoomControl: true}'>
+      <l-map :zoom="zoom" ref="mymap" :center="L.latLng(lat, lng)" style="width: 100%; height: 500px" :options='{zoomControl: true}'>
         <l-tile-layer :url="url"></l-tile-layer>
         <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.latlng">
           <l-popup :content="item.content"></l-popup>
         </l-marker>
       </l-map>
     </div>
-    <!-- <div style="height: 10%; overflow: auto;">
-      <h3>Simple map</h3>
-      Marker is placed at {{ marker.lat }}, {{ marker.lng }}
+
+    <div class="sidebar-container">
+      <map-sidebar />
     </div>
-    <l-map style="height: 90%" :zoom="zoom" :center="center">
-      <l-tile-layer :url="url"></l-tile-layer>
-      <l-marker :lat-lng="marker"></l-marker>
-    </l-map> -->
   </div>
 </template>
 
 <script>
+import L from 'leaflet';
 import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
+import MapSidebar from './MapSidebar.vue';
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LMarker,
-    LPopup
+    LPopup,
+    MapSidebar
   },
   data() {
     return {
-      zoom:13,
-      center: L.latLng(47.413220, -1.219482),
-      url:'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 13,
+      lat: 39.765477,
+      lng: -86.164100,
+      url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       marker: L.latLng(47.413220, -1.219482),
-      markers: []
-    }
+      markers: [],
+      L
+    };
   },
   methods: {
     onPan(e) {
       console.log(e);
-    }
+    },
   },
+  // computed: {
+  //   center: function () {
+  //     return L.latlng(this.lat, this.lng);
+  //   }
+  // },
   mounted() {
-  	const map = this.$refs.mymap.mapObject;
-    map.addControl(new L.Control.Fullscreen());
-  }
-}
+    const L = L;
+  	this.$getLocation({
+      enableHighAccuracy: true
+    })
+      .then(coordinates => {
+        console.log(coordinates);
+        this.lat = coordinates.lat;
+        this.lng = coordinates.lng;
+      });
+  },
+};
 </script>
