8ce7737dfbc6d162268a418f3212312a7a1665ea
Daniel Lomelino <dlomelino@gmail.com>
2018-06-16 11:53:04 -0400

    More progress on User pages, with Google Maps

--- food-app/src/views/user/UserContainer.vue
+++ food-app/src/views/user/UserContainer.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="user-container">
     <div class="top-row grid-x grid-padding-x align-center">
-      <div class="cell shrink grid-x" style="border: 2px solid red">
-        <div class="cell">
-          <h3>I am a:</h3>
+      <div class="cell shrink grid-x">
+        <div class="cell medium-8">
           <div class="mobile-app-toggle" data-mobile-app-toggle>
             <button :class="[currentUserType === 'consumer' ? 'is-active' : '', 'button']"
               @click="switchUser('consumer')">Consumer
@@ -18,11 +17,39 @@
         </div>
       </div>
     </div>
+
+    <div class="grid-x grid-padding-x grid-padding-y align-center">
+
+      <!-- Consumer content -->
+      <Consumer v-if="currentUserType === 'consumer'" />
+
+      <!-- Gardener content -->
+      <Gardener v-else-if="currentUserType === 'gardener'" />
+
+      <!-- Volunteer content -->
+      <Volunteer v-else-if="currentUserType === 'volunteer'" />
+
+      <!-- Default content -->
+      <div class="cell medium-8" v-else>
+      <h3>Choose a user type</h3>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+import Consumer from '@/views/user/Consumer.vue';
+import Gardener from '@/views/user/Gardener.vue';
+import Volunteer from '@/views/user/Volunteer.vue';
+
 export default {
+  components: {
+    Consumer,
+    Gardener,
+    Volunteer,
+  },
   data() {
     return {
       currentUserType: '',
---
+++ food-app/src/views/user/Volunteer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid-container fluid" style="border: 2px solid yellow">
+
+    <div class="grid-x">
+      <div class="cell medium-3" style="border: 2px solid red">
+      left
+      </div>
+
+      <div class="cell medium-8" style="border: 2px solid blue">
+        <GmapMap
+          :center="{lat:10, lng:10}"
+          :zoom="7"
+          map-type-id="terrain"
+          style="width: 500px; height: 300px"
+        >
+          <GmapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+          />
+        </GmapMap>
+      </div>
+
+    </div>
+  </div>
+  </section>
+</template>
